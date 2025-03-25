02c9a353fc35270d4bc25cfd976502b1cfdb2ea1
CaioFillipe <162356041+CaioFillipe@users.noreply.github.com>
2025-03-25 16:33:55 -0300

    Update js.js

--- js.js
+++ js.js
@@ -4,3 +4,9 @@ let divReceitaEl = document.querySelector("#receita");
 botaoEl.addEventListener("click", () => {
     divReceitaEl.classList.toggle("sumido");
 }); 
+
+let selecteL = document.querySelector("#selectzinho");
+
+selecteL.addEventListener("change", () => {
+    divReceitaEl.classList.add("sumido");
+})
